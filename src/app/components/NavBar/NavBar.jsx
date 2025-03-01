7f547eaac123e95dfff3d63703662ea2f3ad8577
lisorthman <lisorthman@gmail.com>
2025-03-01 21:43:32 +0530

    final signup signin

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -33,9 +33,14 @@ function NavBar() {
 
       {/* Search Bar */}
       <div className={styles.searchBar}>
+        <svg className={styles.searchIcon} viewBox="0 0 24 24">
+          <path
+            d="M21.53 20.47l-4.8-4.8a8.5 8.5 0 10-1.06 1.06l4.8 4.8a.75.75 0 101.06-1.06zM10.5 17a6.5 6.5 0 110-13 6.5 6.5 0 010 13z"
+          />
+        </svg>
         <input
           type="search"
-          placeholder="Search..."
+          placeholder="Search Bar"
           className={styles.searchInput}
         />
       </div>
